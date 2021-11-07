852deb9162e4c9c1464516fb04afde5ffe5155b9
codefeak <codefever17@gmail.com>
2021-11-07 18:38:23 +0200

    linter used and formatted

--- force-app/main/default/lwc/product/product.js
+++ force-app/main/default/lwc/product/product.js
@@ -1,37 +1,37 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement } from "lwc";
 
 export default class Product extends LightningElement {
-    @api productFromParent
-    imageUrl;
-    name;
-    price;
-    category;
-    width;
-    heelHeight;
-    gender;
-    brand;
-    size;
-    color;
+  @api productFromParent;
+  imageUrl;
+  name;
+  price;
+  category;
+  width;
+  heelHeight;
+  gender;
+  brand;
+  size;
+  color;
 
+  connectedCallback() {
+    this.setValues(this.productFromParent);
+  }
 
-    connectedCallback()
-    {this.setValues(this.productFromParent)};
-    
-    setValues(value) {
-        this.imageUrl = value.imageUrl__c;
-        this.name = value.Name;
-        this.price = value.price__c;
-        this.category = value.category__c;
-        this.width = value.width__c.slice(0,1);
-        this.heelHeight = value.heelHeight__c;
-        this.gender = value.gender__c;
-        this.brand = value.brand__c;
-        this.size = value.size__c;
-        this.color = value.color__c;
-    }
+  setValues(value) {
+    this.imageUrl = value.imageUrl__c;
+    this.name = value.Name;
+    this.price = value.price__c;
+    this.category = value.category__c;
+    this.width = value.width__c.slice(0, 1);
+    this.heelHeight = value.heelHeight__c;
+    this.gender = value.gender__c;
+    this.brand = value.brand__c;
+    this.size = value.size__c;
+    this.color = value.color__c;
+  }
 
-    handleOnClick() {
-        console.log('i reached here');
-        console.log(this.productFromParent.imageUrl__c)
-    }
+  handleOnClick() {
+    console.log("i reached here");
+    console.log(this.productFromParent.imageUrl__c);
+  }
 }
--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -1,11 +1,13 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 // import getProducts from '@salesforce/apex/ProductController.getProducts';
 
 export default class ProductList extends LightningElement {
-    @api productsFromParent;
+  @api productsFromParent;
 
-    handleOnClick(e) {
-        const productId = e.currentTarget.dataset.value;
-        this.dispatchEvent(new CustomEvent('productclicked', {detail: productId}));
-    }
+  handleOnClick(e) {
+    const productId = e.currentTarget.dataset.value;
+    this.dispatchEvent(
+      new CustomEvent("productclicked", { detail: productId })
+    );
+  }
 }
