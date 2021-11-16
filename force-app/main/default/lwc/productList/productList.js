f968c4ec742a874341c6700ba454dba1296a4c9a
codefeak <codefever17@gmail.com>
2021-11-16 14:21:25 +0200

    change css in header for single page app

--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -1,7 +1,9 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 // import getProducts from '@salesforce/apex/ProductController.getProducts';
 
 export default class ProductList extends LightningElement {
+  @api productsFromParent;
+
   handleOnClick(e) {
     const productId = e.currentTarget.dataset.value;
     this.dispatchEvent(
